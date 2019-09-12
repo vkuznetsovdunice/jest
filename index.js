d59a8ea959e533e5c2ba136bdb640968eb2a91a2
Valeriy Kuznetsov <v.kuznetsov@dunice.net>
2019-09-12 16:21:35 +0300

    divided the test block into 2 parts

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-export const a = [{ a: 1, b: -1 }, { a: 2, b: -2 }, { a: 3, b: -3 }, { a: 4, b: -4 }, { a: 5, b: -5 }];
-
-export const filters = [
-  {
-    key: 'a',
-    cond: 'gt',
-    gt: 2,
-  },
-  {
-    key: 'b',
-    cond: 'lt',
-    lt: -3,
-  },
-];
-
-export const sort = [
-  {
-    key: 'a',
-    asc: true,
-  },
-  {
-    key: 'b',
-    asc: false,
-  },
-];
-
 export const myFunction = (array, filters, sort) => {
   const _filterConds = {
     gt(key, value, item) {
@@ -35,7 +9,8 @@ export const myFunction = (array, filters, sort) => {
   };
 
   const _filters = filters.map((filter) => _filterConds[filter.cond].bind(null, filter.key, filter[filter.cond]));
-  return a.filter((item) => {
+  // a was global defined and using global 'a' array, change 'a' -> 'array'
+  return array.filter((item) => {
     return _filters.every((filter) => filter(item));
   })
 };
--- index.test.js
+++ index.test.js
@@ -1,67 +1,18 @@
-import { myFunction, a, filters, sort } from './index';
-
-// result with correct data
-const result = [
-    { 
-        a: 4, 
-        b: -4,
-    }, 
-    { 
-        a: 5, 
-        b: -5, 
-    },
-]
-
-// the original array was sorted, stretch the array with the mixed positions of the objects
-const testArray = [
-    { 
-        a: 2, 
-        b: -2, 
-    }, 
-    { 
-        a: 5, 
-        b: -5, 
-    }, 
-    { 
-        a: 3, 
-        b: -3, 
-    }, 
-    { 
-        a: 1, 
-        b: -1,
-    }, 
-    { 
-        a: 4, 
-        b: -4, 
-    },
-];
-
-// array with null values
-const testArrayWithNull = [
-    { 
-        a: 2, 
-        b: null, 
-    }, 
-    { 
-        a: 5, 
-        b: -5, 
-    }, 
-    { 
-        a: null, 
-        b: -3, 
-    }, 
-    { 
-        a: 1, 
-        b: -1, 
-    }, 
-    { 
-        a: null, 
-        b: -4, 
-    },
-];
+import { myFunction } from './index';
 
 
 describe('Function myFunction', () => {
+    // result with correct data
+    const result  = [{ a: 4, b: -4, }, { a: 5, b: -5, },]
+    const a       = [{ a: 1, b: -1, }, { a: 2, b: -2, }, { a: 3, b: -3, }, { a: 4, b: -4, }, { a: 5, b: -5, }];
+    const filters = [{ key: 'a', cond: 'gt', gt: 2, }, { key: 'b', cond: 'lt', lt: -3,},];
+    const sort    = [{ key: 'a', asc: true,}, {key: 'b', asc: false,},];
+
+    Object.freeze(a);
+    Object.freeze(filters);
+    Object.freeze(sort);
+    Object.freeze(result);
+    
     test('call with an empty array, if for some reason it is not populated', () => {
         expect(myFunction([], filters, sort)).toBeDefined();
     });
@@ -73,21 +24,33 @@ describe('Function myFunction', () => {
     test("Array sort don used in function, call with empty array don't change the result", () => {
         expect(myFunction(a, filters, [])).toEqual(result);
     });
-    
+})
+
+describe('Function myFunction', () => {
+    // result with correct data
+    const result  = [{ a: 4, b: -4, }, { a: 5, b: -5, },]
+    const a       = [{ a: 1, b: -1, }, { a: 2, b: -2, }, { a: 3, b: -3, }, { a: 4, b: -4, }, { a: 5, b: -5, }];
+    const filters = [{ key: 'a', cond: 'gt', gt: 2, }, { key: 'b', cond: 'lt', lt: -3,},];
+    const sort    = [{ key: 'a', asc: true,}, {key: 'b', asc: false,},];
+
+    Object.freeze(a);
+    Object.freeze(filters);
+    Object.freeze(sort);
+    Object.freeze(result);
+
     test('call with correct data. Cleanliness check', () => {
-        const localResult = myFunction([], filters, sort);
-        expect(myFunction([], filters, sort)).toEqual(localResult);
-    });
-    
-    test('call for testing of sort', () => {
-        expect(myFunction(testArray, filters, sort)).toEqual(result);
+        const localResult = myFunction(a, filters, sort);
+        expect(myFunction(a, filters, sort)).toEqual(localResult);
     });
     
     test('call with array where some of fields is null', () => {
+        // array with null values
+        const testArrayWithNull = [{ a: 2, b: null, }, { a: 5, b: -5, }, { a: null, b: -3, }, { a: 1, b: -1, }, { a: null, b: -4, },];
         expect(myFunction(testArrayWithNull, filters, sort)).toBeDefined();
     });
     
     test('call with filters where some of fields in array is null', () => {
+        const filtersWithNull = [{ key: 'a', cond: 'gt', gt: null,}, { key: 'b', cond: null, lt: -3,},];
         expect(() => { myFunction(a, filtersWithNull, sort) }).toThrow();
     });
 })
